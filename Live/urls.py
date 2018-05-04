4f2cbfa726ed1b517aa84d22a6e21e43f5973d72
Gavin Mcrey Canete <gavinmcrey@gmail.com>
2018-05-04 15:33:21 +0800

    Auction Livestream

--- Live/urls.py
+++ Live/urls.py
@@ -1,4 +1,4 @@
-"""auction URL Configuration
+"""Live URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -13,12 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-	path('login/', include('login.urls')),
+	path('lvs/', include('lvs.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
